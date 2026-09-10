910517318c80f28e95601fcb4d77e29a4291d37a
sajadkhavas <144109918+sajadkhavas@users.noreply.github.com>
2026-09-10 21:41:30 +0330

    feat: add 3D model admin list page

---
+++ app/Filament/Resources/ProductModel3dResource/Pages/ListProductModel3ds.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\ProductModel3dResource\Pages;
+
+use App\Filament\Resources\ProductModel3dResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListProductModel3ds extends ListRecords
+{
+    protected static string $resource = ProductModel3dResource::class;
+}
